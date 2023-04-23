3bd1d4210c6f01602d600a52e4d33ec60d2f3fb3
KRITTIKA0422 <krittika489podder@gmail.com>
2023-04-23 11:20:48 +0530

    assigned delete button to assign mentor

--- src/Assignmentor.js
+++ src/Assignmentor.js
@@ -3,7 +3,7 @@ import "./Assignmentor.css";
 import {useState} from "react";
 import { useEffect } from "react";
 import { API2 } from "./global";
-
+import { Button } from "@mui/material";
 
 export default function Assignmentor(){
 
@@ -16,19 +16,26 @@ export default function Assignmentor(){
      .then((s)=>setAssigns (s));
    };
     useEffect(()=> getAssigns(),[]);
+    const deleteassign=(id)=>{
+      fetch(`${API2}/assign/${id}`,{
+          method:'DELETE',
+      }).then((data)=>data.json())
+      .then(()=>getAssigns());
+       };
  
   
     return (
-     <div className="assignment-list-container">{assigns.map((s)=>(<Assigndata assigning ={s}/>))}</div>
+     <div className="assignment-list-container">{assigns.map((s)=>(<Assigndata assigning ={s} deleteButton={<Button variant="contained" color="error" onClick={()=>deleteassign(s.id)}><i className="material-icons">delete</i></Button>}/>))}</div>
   );
 }
 
-function Assigndata({assigning}){
+function Assigndata({assigning,deleteButton}){
 return(
   <div className="assignment-container">
     <p>Assign Id no.: {assigning.assignid}</p>
     <p>Student Name: {assigning.studentname}</p>
     <p>Mentor Name: {assigning.mentorname}</p>
+    {deleteButton}
   </div>
 );
 }
